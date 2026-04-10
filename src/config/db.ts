bd9169cf426780caf9cd6d6952082fc03066852f
AKSHAY-PV-8 <akshaypv19@gmail.com>
2026-04-10 12:39:31 +0530

    refactor

--- src/config/db.ts
+++ src/config/db.ts
@@ -3,12 +3,17 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+const isProduction = process.env.NODE_ENV === "production";
+
+
 export const pool = new Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     port: Number(process.env.DB_PORT),
-    ssl: { rejectUnauthorized: false }
+    ssl: isProduction
+    ? { rejectUnauthorized: false }
+    : false,
 })
 
